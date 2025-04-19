ff71220f080aaaee22539f06de9979c710cf8bf8
McHolguin990820 <marianacholguinp@outlook.com>
2025-04-19 14:00:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="text-center mt-10 text-xl text-indigo-800">
-      <h1>🎉 Bienvenido a Finanzas Conscientes</h1>
+      <h1>Bienvenido a Finanzas Conscientes</h1>
       <p>Simula tu préstamo y toma decisiones informadas.</p>
     </div>
   );
